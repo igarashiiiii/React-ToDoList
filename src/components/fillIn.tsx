c5932282fa996e713cd89c512b9558f0400edb1b
Hayato <igahaya5@gmail.com>
2022-05-05 08:59:44 +0900

    "yarn add @mui/icons-material"

--- src/components/fillIn.tsx
+++ src/components/fillIn.tsx
@@ -1,5 +1,23 @@
 import React from 'react';
 
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import ListItemText from '@mui/material/ListItemText';
+import TextField from '@mui/material/TextField';
+import DoneOutlineOutlinedIcon from '@mui/icons-material/DoneOutlineOutlined';
+
 export const FillIn = () => {
-  return <div>FillIn</div>;
+  return (
+    <>
+      <TextField id="standard-basic" label="Standard" variant="standard" />
+      <Button variant="outlined">Add</Button>
+      <Box>
+        <List>
+          <ListItemText primary="Trash" />
+          <DoneOutlineOutlinedIcon></DoneOutlineOutlinedIcon>
+        </List>
+      </Box>
+    </>
+  );
 };
--- src/components/yet.tsx
+++ src/components/yet.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export const Yet = () => {
-  return <div>Yetaaaaaaaaaaaaaaaaaaaaa</div>;
+  return <div>Yet</div>;
 };
