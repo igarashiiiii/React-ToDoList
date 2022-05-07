8dac0b47a0495c93735c16bba18832dcf69578c3
Hayato <igahaya5@gmail.com>
2022-05-07 19:06:12 +0900

    "add global components"

--- src/components/fillIn.tsx
+++ src/components/fillIn.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState , useContext} from 'react';
+import { Lists } from '../gobalComponents/globalComponents';
 
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
@@ -7,17 +8,29 @@ import ListItemText from '@mui/material/ListItemText';
 import TextField from '@mui/material/TextField';
 import DoneOutlineOutlinedIcon from '@mui/icons-material/DoneOutlineOutlined';
 
+
 export const FillIn = () => {
+  
+  //objの型定義any以外にしたい
+  const obj:any = useContext(Lists);
+  const [thing, setThing] = useState("");
+  const [addthing, setAddThing] = useState("");
+  const onClickText = () => {
+    setAddThing(thing);
+  }
+  console.log(obj.info)
   return (
-    <>
-      <TextField id="standard-basic" label="Standard" variant="standard" />
-      <Button variant="outlined">Add</Button>
+    <div>
+      
+      <TextField id="standard-basic" label="Standard" variant="standard" onChange={(event) => setThing(event.target.value)}/>
+      <Button variant="outlined" onClick={onClickText}>Add</Button>
       <Box>
         <List>
           <ListItemText primary="Trash" />
           <DoneOutlineOutlinedIcon></DoneOutlineOutlinedIcon>
         </List>
       </Box>
-    </>
+      <p>{addthing}</p>
+    </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { RouterConfig } from './route/routing';
+import { ListsProvider } from "./gobalComponents/globalComponents"
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container!);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <RouterConfig />
+      <ListsProvider>
+        <RouterConfig />
+      </ListsProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
